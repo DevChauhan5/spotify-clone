1485e78d67c323a12162d9d8124e07baa79eb310
Dev Chauhan <124146292+DevChauhan5@users.noreply.github.com>
2023-06-01 13:25:16 +0000

    Update 5 files

--- components/Box.tsx
+++ components/Box.tsx
@@ -1,5 +1,26 @@
-const Box = () => {
-  return <div>Box</div>;
+import { twMerge } from "tailwind-merge";
+
+interface BoxProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Box: React.FC<BoxProps> = ({ children, className }) => {
+  return (
+    <div
+      className={twMerge(
+        `
+        bg-neutral-900 
+        rounded-lg 
+        h-fit 
+        w-full
+        `,
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Box;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,6 +3,8 @@ import { usePathname } from "next/navigation";
 import { useMemo } from "react";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import Box from "./Box";
+import SidebarItem from "./SidebarItem";
 
 interface SidebarProps {
   children: React.ReactNode;
@@ -42,9 +44,21 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
           p-2
         "
       >
-        <Box>Sidebar Nav</Box>
+        <Box>
+          <div className="flex flex-col gap-y-4 px-5 py-4">
+            {routes.map((item) => (
+              <SidebarItem key={item.label} {...item} />
+            ))}
+          </div>
+        </Box>
+        <Box className="overflow-y-auto h-full">
+          Library songs
+        </Box>
       </div>
-    </div>
+      <main className="h-full flex-1 overflow-y-auto py-2">
+        {children}
+      </main>
+      </div>
   );
 };
 
